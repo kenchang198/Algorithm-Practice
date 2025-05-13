cec86bba6bb9e86093f11693eefa3b6d01e36a79
kenchang198 <sasamotoken@gmail.com>
2025-05-13 20:19:11 +0900

    move_zeros 関数化

--- move_zeros/move_zeros.php
+++ move_zeros/move_zeros.php
@@ -1,18 +1,22 @@
 <?php
 
-$nums = [0, 1, 0, 3, 12];
-
 // 0以外の数字を左にもっていく方法
-$left = 0;
-foreach ($nums as $k => $num) {
-    if ($num !== 0) {
-        $nums[$left] = $num;
-        $left += 1;
+function move_zeros($nums) {
+    $left = 0;
+    foreach ($nums as $k => $num) {
+        if ($num !== 0) {
+            $nums[$left] = $num;
+            $left += 1;
+        }
     }
-    // var_dump($left);
+
+    for ($i = $left; $i < count($nums); $i++) {
+        $nums[$i] = 0;
+    }
+    return $nums;
 }
 
-for ($i = $left; $i < count($nums); $i++) {
-    $nums[$i] = 0;
-}
-var_dump($nums);
+$nums = [0, 1, 0, 3, 12];
+// $nums = [0, 0, 0, 0, 0, 0, 9, 0, 9];
+
+var_dump(move_zeros($nums));
