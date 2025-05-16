8d7cc24b0abff69b4e2f7818135402ffae2df7ee
kenchang198 <sasamotoken@gmail.com>
2025-05-16 13:40:43 +0900

    [WIP] move_zeros python

---
+++ move_zeros/move_zeros.py
@@ -0,0 +1,13 @@
+
+nums = [0, 1, 0, 3, 12]
+
+left = 0
+
+for num in nums:
+    if num != 0
+    nums[left] = num
+    left += 1
+
+# print(len(nums))
+# for l in range(left, len(nums)):
+#     print(l)
