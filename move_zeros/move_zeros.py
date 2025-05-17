d715aad022c1015e515d3d3454a0a345a03479d3
kenchang198 <sasamotoken@gmail.com>
2025-05-17 11:57:22 +0900

    py move_zeros not func

--- move_zeros/move_zeros.py
+++ move_zeros/move_zeros.py
@@ -1,13 +1,17 @@
 
 nums = [0, 1, 0, 3, 12]
+print(nums)
 
 left = 0
 
 for num in nums:
-    if num != 0
-    nums[left] = num
-    left += 1
+    if num != 0:
+        nums[left] = num
+        left += 1
 
-# print(len(nums))
-# for l in range(left, len(nums)):
-#     print(l)
+print(nums)
+
+for l in range(left, len(nums)):
+    nums[l] = 0
+
+print(nums)
