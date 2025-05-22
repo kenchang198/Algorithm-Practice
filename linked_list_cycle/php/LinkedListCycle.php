56fde24e0c70cd74bcef4bfefa91bf0e9dc9bb1b
kenchang198 <sasamotoken@gmail.com>
2025-05-22 09:41:50 +0700

    ListNode PHP

--- linked_list_cycle/php/LinkedListCycle.php
+++ linked_list_cycle/php/LinkedListCycle.php
@@ -5,8 +5,6 @@ class ListNode
     public function __construct(public $val = 0, public $next = null){}
 }
 
-$obj = new ListNode();
-
 function hasCycle($head)
 {
     // 空リストまたは1つのノードしかない場合はサイクルなし
@@ -18,5 +16,32 @@ function hasCycle($head)
     $slow = $head;
     $fast = $head;
 
-    // while
+    while ($fast !== null && $fast->next !== null) {
+        // 遅いポインタは1ステップ進む
+        $slow = $slow->next;
+        // 速いポインタは2ステップ進む
+        $fast = $fast->next->next;
+        
+        // 両ポインタが同じノードを指していればサイクルが存在する
+        if ($slow === $fast) {
+            return true;
+        }
+    }
+
+    // リストの終端に達したのでサイクルはない
+    return false;
 }
+
+// サイクルのないリンクドリスト: 1->2->3->NULL
+$node1 = new ListNode(1);
+$node2 = new ListNode(2);
+$node3 = new ListNode(3);
+$node1->next = $node2;
+$node2->next = $node3;
+
+echo "サイクルなし: " . (hasCycle($node1) ? "true" : "false") . "\n";
+
+// サイクルのあるリンクドリスト: 1->2->3->1
+$node3->next = $node1; // 3が1を指すようにしてサイクルを作る
+
+echo "サイクルあり: " . (hasCycle($node1) ? "true" : "false") . "\n";
