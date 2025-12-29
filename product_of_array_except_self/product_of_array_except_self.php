e75e12c3f48803be0ca984550a2055d1a9a9b516
kenchang198 <sasamotoken@gmail.com>
2025-12-29 11:49:12 +0900

    [Product of Array Except Self ] space optimized

--- product_of_array_except_self/product_of_array_except_self.php
+++ product_of_array_except_self/product_of_array_except_self.php
@@ -21,9 +21,30 @@ function productOfArrayExceptSelf(array $nums): array
     return $answer;
 }
 
-$nums = [1,2,3,4]; // output 24,12,8,6]
+function spaceOptimized(array $nums): array
+{
+    $n = count($nums);
+    $answer = [];
+
+    // 第1パス: 左側の積 (prefix)
+    $answer[0] = 1;
+    for ($i = 1; $i < $n; $i++) {
+        $answer[$i] = $answer[$i - 1] * $nums[$i - 1];
+    }
+    
+    // 第2パス: 右側の積（suffix）を掛ける
+    $rightProduct = 1;
+    for ($i = $n - 1; $i >= 0; $i--) {
+        $answer[$i] = $answer[$i] * $rightProduct;
+        $rightProduct = $rightProduct * $nums[$i];
+    }
+    return $answer;
+}
+
+$nums = [1,2,3,4]; // output [24,12,8,6]
 $nums = [-1,1,0,-3,3]; // [0,0,9,0,0]
 $nums = [2,2,3,4]; // [24, 24, 16, 12]
 
-$r = productOfArrayExceptSelf($nums);
+// $r = productOfArrayExceptSelf($nums);
+$r = spaceOptimized($nums);
 echo json_encode($r) . "\n";
